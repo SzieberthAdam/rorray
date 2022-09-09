90a462bf7f7a3c920c783e3bfcf241b42574632a
Szieberth Ádám <sziebadam@gmail.com>
2022-09-09 18:21:14 +0200

    scenario.frm new format

--- devutils/frm2ror.py
+++ devutils/frm2ror.py
@@ -20,13 +20,13 @@ INT_TYPES = (
 
 def str2val(s, type_=None):
     if type_ in INT_TYPES:
-        val = str2int(s)
+        val = str2int(s, type_=type_)
     else:
         val = json.loads(s)
     return val
 
 
-def str2int(s):
+def str2int(s, type_=None):
     s = s.lower()
     if s.startswith("0b"):
         v = int(s, 2)
@@ -34,66 +34,16 @@ def str2int(s):
         v = int(s, 16)
     else:
         v = int(s)
+    if type_ == "uint8_t":
+        assert v in range(0, 256)
+    elif type_ == "uint16_t":
+        assert v in range(0, 65536)
     return v
 
+ALIGN = 4
 
-def frmparse(frmstr):
-    mode = ("0",)
-    d = {"ANCHOR": {}}
-    i = 0
-    L = frmstr.split("\n")
-    while i < len(L):
-        sline = L[i].partition(";")[0].strip() # trim comments
-        if not sline:
-            i += 1
-            continue
-        if mode[0] == "0":
-            if sline.startswith("FILESIGNATURE"):
-                d["FILESIGNATURE"] = str(json.loads(sline.split(" ")[1]))
-            elif sline.startswith("FILEVERSION"):
-                _, type_, valstr = sline.split(" ")
-                val = str2val(valstr, type_)
-                d["FILEVERSIONTYPE"] = type_
-                d["FILEVERSION"] = val
-            elif sline.startswith("ADDRTYPE"):
-                _, d["ADDRTYPE"] = sline.split(" ")
-            elif sline.startswith("ALIGN"):
-                _, valstr = sline.split(" ")
-                val = str2int(valstr)
-                d["ALIGN"] = val
-            elif sline.startswith("ANCHOR"):
-                _, anchor = sline.split(" ")
-                danchor = d["ANCHOR"][anchor] = {"VAR": {}}
-                mode = ("ANCHOR", anchor)
-        elif mode[0] == "ANCHOR":
-            if sline.startswith("ITEM"):
-                _, valstr = sline.split()
-                val = str2int(valstr)
-                danchor["ITEM"] = val
-            elif sline.startswith("VAR"):
-                _t = sline.split(" ")
-                _, var, type_ = _t[:3]
-                dval = danchor["VAR"][var] = {"TYPE": type_, "VAL": None}
-                if _t[3:]:
-                    valstr = " ".join(_t[3:]).lstrip()
-                    val = json.loads(f'[{valstr.rstrip(",")}]')
-                    assert type(val) is list
-                    dval["VAL"] = val
-                    if valstr.endswith(","):
-                        mode = ("VAR", anchor, var)
-                    else:
-                        if dval["VAL"][-1] == "*":
-                            lastval = dval["VAL"][-2]
-                            dval["VAL"].extend([lastval] * (danchor["ITEM"] - len(dval["VAL"])))
-                        assert len(dval["VAL"]) == danchor["ITEM"]
-                        mode = ("ANCHOR", anchor)
-                else:
-                    mode = ("VAR", anchor, var)
-#           elif sline.startswith("ADDR"):
-#               _t = sline.split(" ")
-#               daddr = danchor["ADDR"] = None
-        i += 1
-    return d
+GROUPDEF_STRUCTFMT = "=HHL"
+ATTRDEF_STRUCTFMT = "=BBHL"
 
 
 if __name__ == "__main__":
@@ -113,78 +63,234 @@ if __name__ == "__main__":
         frmstr = _f.read()
 
 
+    ba = bytearray() # this is the whole data which consist of:
+    ba_header = bytearray()    # - header (padded to 32 bytes)
+    ba_groupdef = bytearray()  # - group definitions (16 bytes each)
+    ba_attrdef = bytearray()   # - group attribute definitions (8 bytes each; block paddded to 16 bytes)
+    ba_attrvals = bytearray()  # - area of attribute values (area padded to 16 bytes)
 
-    mode = ("0",)
-    d = {"ANCHOR": {}}
-    i = 0
-    L = frmstr.split("\n")
-    while i < len(L):
-        sline = L[i].partition(";")[0].strip() # trim comments
-        print([i, mode, sline])
-        if not sline:
+    # objects used:
+    group = None       # b'name'
+    groupdef = None    # [b'name', itemcount:uint16, attrcount:uit16, firstitemlink:uint32, firstattrtablelink:uint32]
+    groupidx = 0       # Group ID
+    groupinfo = {}     # {b'name': {"idx": groupidx, "def": groupdef}}
+    attrdef = None     # [b'nam', attrtype:uint8, firstattrlink:uint32]
+    attridx = 0        # attribute index
+    vals = None        # [val1, val2, ...]
+    
+    mode = ("FILESIGNATURE",)  # loop control
+ 
+    for li0 in frmstr.split("\n"):
+        li = li0.partition(";")[0].strip() # trim comments
+        print([i, mode, li])
+        if not li:
             i += 1
             continue
-        if mode[0] == "0":
-            if sline.startswith("FILESIGNATURE"):
-                d["FILESIGNATURE"] = str(json.loads(sline.split(" ")[1]))
-            elif sline.startswith("FILEVERSION"):
-                _, type_, valstr = sline.split(" ")
-                val = str2val(valstr, type_)
-                d["FILEVERSIONTYPE"] = type_
-                d["FILEVERSION"] = val
-            elif sline.startswith("ADDRTYPE"):
-                _, d["ADDRTYPE"] = sline.split(" ")
-            elif sline.startswith("ALIGN"):
-                _, valstr = sline.split(" ")
-                val = str2int(valstr)
-                d["ALIGN"] = val
-            elif sline.startswith("ANCHOR"):
-                _, anchor = sline.split(" ")
-                danchor = d["ANCHOR"][anchor] = {"VAR": {}}
-                mode = ("ANCHOR", anchor)
-        elif mode[0] == "ANCHOR":
-            if sline.startswith("ITEM"):
-                _, valstr = sline.split(" ")
-                val = str2int(valstr)
-                danchor["ITEM"] = val
-            elif sline.startswith("VAR"):
-                _t = sline.split(" ")
-                _, var, type_ = _t[:3]
-                dval = danchor["VAR"][var] = {"TYPE": type_, "VAL": None}
+        if mode[0] == "FILESIGNATURE":
+            assert li.startswith("FILESIGNATURE ")
+            _s = str(json.loads(li.split(" ")[1]))
+            _b = _s.encode("ascii")
+            assert len(_b) == 3
+            ba_header.extend(_b)
+            mode = ("FILEVERSION",)
+            continue
+        if mode[0] == "FILEVERSION":
+            assert li.startswith("FILEVERSION ")
+            _v = str2val(li.split(" ")[1], "uint8_t")
+            ba_header.append(_v)
+            mode = ("NEWGROUP",)
+            continue
+        if mode[0] == "NEWGROUP":
+            assert li.startswith("NEWGROUP ")
+            _, group, _items, _attrs = li.split(" ")
+            group = group.encode("ascii")
+            _items = str2int(_items)
+            _attrs = str2int(_attrs)
+            groupdef = [_items, _attrs, len(ba_attrvals)]
+            _b = struct.pack(GROUPDEF_STRUCTFMT, *groupdef)
+            ba_groupdef.extend(_b)
+            groupidx += 1
+            attridx = 0
+            mode = ("ITEMS", group)
+            continue
+        if mode[0] == "ITEMS":
+            assert li.startswith("ITEMS ")
+            assert mode[1] == groupdef[0]
+            _v = str2int(li.split(" ")[1])
+            groupdef[1] = _v
+            mode = ("ATTRS", group)
+            continue
+        if mode[0] == "ATTRS":
+            assert li.startswith("ATTRS ")
+            assert mode[1] == groupdef[0]
+            _v = str2int(li.split(" ")[1])
+            groupdef[2] = _v
+            _b = struct.pack(GROUPDEF_STRUCTFMT, *groupdef)
+            ba_groupdef.extend(_b)
+            attridx = 0
+            mode = ("ATTR", group)
+            continue
+        if mode[0] == "ATTR":
+            assert li.startswith("ATTR ")
+            _t = li.split(" ")
+            _, _attr, _attrtype = _t[:3]
+            _attr = _attr.encode("ascii")
+            assert len(_attr) == 3
+            attrdef = [_attr, _attrtype, len(ba_attrvals)]
+            _b = struct.pack(ATTRDEF_STRUCTFMT, *attrdef)
+            ba_attrdef.extend(_b)
+            vals = []
+            mode = ("ATTRVALS", group, _attr)
+            if not _t[3:]:
+                continue
+            else:
+                li = " ".join(_t[3:]).lstrip() # ATTENTION! SHORTCUT!
+        if mode[0] == "ATTRVALS":
+            _v = json.loads(f'[{li.rstrip(",")}]')
+            vals.extend(_v)
+            assert len(vals) <= groupdef[1]
+            if len(vals) < groupdef[1]:
+                assert li.endswith(",")
+                continue
+            elif len(vals) == groupdef[1]:
+
+
+
+
+
+
+
+
+
+
+                elif li.endswith("*"):
+                    vals = vals[:-1]
+                    vals.extend([vals[-1]] * (groupdef[1] - len(vals)))
+
+
+                continue
+
+
+
+
+
+
+
+            attrtableitem = 
+
+            assert mode[1] == groupdef[0]
+            assert li.startswith("ATTR ") or li.startswith("LINK ")
+            if li.startswith("ATTR "):
+
+
+                raise Exception
+
+                dval = dgroup["ATTR"][attr] = {"TYPE": type_, "VAL": None}
                 if _t[3:]:
                     valstr = " ".join(_t[3:]).lstrip()
                     val = json.loads(f'[{valstr.rstrip(",")}]')
                     assert type(val) is list
                     dval["VAL"] = val
                     if valstr.endswith(","):
-                        mode = ("VAR", anchor, var)
+                        mode = ("ATTR", group, attr)
                     else:
                         if dval["VAL"][-1] == "*":
                             lastval = dval["VAL"][-2]
-                            dval["VAL"].extend([lastval] * (danchor["ITEM"] - len(dval["VAL"])))
-                        assert len(dval["VAL"]) == danchor["ITEM"]
-                        mode = ("ANCHOR", anchor)
+                            dval["VAL"].extend([lastval] * (dgroup["ITEMS"] - len(dval["VAL"])))
+                        assert len(dval["VAL"]) == dgroup["ITEMS"]
+                        mode = ("NEWGROUP", group)
                 else:
                     dval["VAL"] = []
-                    mode = ("VAR", anchor, var)
-#           elif sline.startswith("ADDR"):
-#               _t = sline.split(" ")
-#               daddr = danchor["ADDR"] = None
-            elif sline.startswith("ANCHOR"):
-                mode = ("0",)
-                continue
-        elif mode[0] == "VAR":
-            valstr = sline.strip()
-            val = json.loads(f'[{valstr.rstrip(",")}]')
-            assert type(val) is list
-            dval["VAL"].extend(val)
-            if not valstr.endswith(","):
-                if dval["VAL"][-1] == "*":
-                    lastval = dval["VAL"][-2]
-                    dval["VAL"].extend([lastval] * (danchor["ITEM"] - len(dval["VAL"])))
-                assert len(dval["VAL"]) == danchor["ITEM"]
-                mode = ("ANCHOR", anchor)
+                    mode = ("ATTR", group, attr)
+            elif li.startswith("LINK "):
+                # done with ATTRs, groupdef is done
+                _b = struct.pack(GROUPDEF_STRUCTFMT, *groupdef)
+                groupdeftbl.extend(_b)
+
+
+
+
+
+                raise Exception
+
+
+
+                links = group, [], bytearray()
+                for s in li[len("LINK "):].split(","):
+                    _t = s.strip().split(".")
+                    if len(_t) == 1:
+                        _t += ["1"]
+                    t[1] = str2int(t[1])
+                    links[1].append(_t)
+                if links[1] == groupdef[1]:
+
+                    mode = ("0",)
+
+        
+
         i += 1
 
-    D = d
-    #D = frmparse(frmstr)
+
+#            if li.startswith("FILESIGNATURE"):
+#                d["FILESIGNATURE"] = str(json.loads(li.split(" ")[1]))
+#            elif li.startswith("FILEVERSION"):
+#                _, type_, valstr = li.split(" ")
+#                val = str2val(valstr, type_)
+#                d["FILEVERSIONTYPE"] = type_
+#                d["FILEVERSION"] = val
+#            elif li.startswith("LINKTYPE"):
+#                _, d["LINKTYPE"] = li.split(" ")
+#            elif li.startswith("ALIGN"):
+#                _, valstr = li.split(" ")
+#                val = str2int(valstr)
+#                d["ALIGN"] = val
+#            elif li.startswith("NEWGROUP"):
+#                _, group = li.split(" ")
+#                dgroup = d["NEWGROUP"][group] = {"ATTR": {}}
+#                mode = ("NEWGROUP", group)
+#        elif mode[0] == "NEWGROUP":
+#            if li.startswith("ITEMS"):
+#                _, valstr = li.split(" ")
+#                val = str2int(valstr)
+#                dgroup["ITEMS"] = val
+#            elif li.startswith("ATTR"):
+#                _t = li.split(" ")
+#                _, attr, type_ = _t[:3]
+#                dval = dgroup["ATTR"][attr] = {"TYPE": type_, "VAL": None}
+#                if _t[3:]:
+#                    valstr = " ".join(_t[3:]).lstrip()
+#                    val = json.loads(f'[{valstr.rstrip(",")}]')
+#                    assert type(val) is list
+#                    dval["VAL"] = val
+#                    if valstr.endswith(","):
+#                        mode = ("ATTR", group, attr)
+#                    else:
+#                        if dval["VAL"][-1] == "*":
+#                            lastval = dval["VAL"][-2]
+#                            dval["VAL"].extend([lastval] * (dgroup["ITEMS"] - len(dval["VAL"])))
+#                        assert len(dval["VAL"]) == dgroup["ITEMS"]
+#                        mode = ("NEWGROUP", group)
+#                else:
+#                    dval["VAL"] = []
+#                    mode = ("ATTR", group, attr)
+##           elif li.startswith("LINK"):
+##               _t = li.split(" ")
+##               dlink = dgroup["LINK"] = None
+#            elif li.startswith("NEWGROUP"):
+#                mode = ("0",)
+#                continue
+#        elif mode[0] == "ATTR":
+#            valstr = li.strip()
+#            val = json.loads(f'[{valstr.rstrip(",")}]')
+#            assert type(val) is list
+#            dval["VAL"].extend(val)
+#            if not valstr.endswith(","):
+#                if dval["VAL"][-1] == "*":
+#                    lastval = dval["VAL"][-2]
+#                    dval["VAL"].extend([lastval] * (dgroup["ITEMS"] - len(dval["VAL"])))
+#                assert len(dval["VAL"]) == dgroup["ITEMS"]
+#                mode = ("NEWGROUP", group)
+#        i += 1
+#
+#    D = d
+#    #D = frmparse(frmstr)
--- devutils/scenario.frm
+++ devutils/scenario.frm
@@ -1,36 +1,56 @@
-FILESIGNATURE "RoR"
-FILEVERSION uint8_t 1
-ADDRTYPE uint32_t
-ALIGN 4
+SIG "RoR"
+VER 1
 
-ANCHOR NULL
-ITEM 1
-ADDR NULL
 
-ANCHOR FACT ; "Faction"
-ITEM 6
-VAR NAM string(20) 
-"Aquila", "Luna Crescens", "Parma", "Corona", "Manus", "Porta"
-ADDR NULL *
+G NULL 1 0
 
-ANCHOR SENA ; "Family Card / Senator"
-ITEM 34
-VAR NUM uint8_t 
- 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 
-11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
-31, 32, 33, 34
-VAR NAM string
-"Cornelius" , "Fabius"    , "Valerius"  , "Julius"    , "Claudius"  ,
-"Manlius"   , "Fulvius"   , "Furius"    , "Aurelius"  , "Junius"    ,
-"Papirius"  , "Acilius"   , "Flaminius" , "Aelius"    , "Sulpicius" ,
-"Calpurnius", "Plautius"  , "Quinctius" , "Aemilius"  , "Terentius" ,
-"Servilius" , "Porcius"   , "Popillius" , "Cassius"   , "Sempronius",
-"Octavius"  , "Marius"    , "Tullius"   , "Licinius"  , "Pompeius"  ,
-"Domitius"  , "Antonius"  , "Caecilius" , "Marcius"
-ADDR 
-FACT.1, FACT.2, FACT.6, FACT.3, FACT.1,
-FACT.2, NULL  , FACT.2, FACT.5, FACT.3,
-FACT.1, FACT.5, FACT.4, FACT.4, FACT.3,
-FACT.6, NULL  , FACT.5, FACT.4, FACT.6,
-NULL, *
+
+G GAME 1 8 ; "Game"
+A NAME string(16) "" ; name
+A STYR uint16_t 0 ; start year
+A STMO uint8_t 0 ; start month
+A STDY uint8_t 0 ; start day
+A STHO uint8_t 0 ; start hour
+A STMI uint8_t 0 ; start minute
+A STSE uint8_t 0 ; start seconds
+A STMS uint16_t 0 ; start milliseconds
+A TURN uint16_t 0 ; turn
+A PHASE uint_t 0 ; phase
+A SUBPHASE uint32_t ; subphase (technical)
+
+
+G PLYR 7 3 ; "Player"
+A GAME group GAME, * ; belongs to GAME (in play) or NULL (not in play)
+A NAME string(16) "Jupiter", "", * ; player name, Jupiter (id=0) does the automated stuff
+A PERM uint8_t 0xFF, 0x03, * ; permissions bit1: can see public stuff, bit2: can see/have own stuff (off for possible spectators); bit3: can see other player's stuff; bit4: can see decks
+
+
+G FACT 6 1 ; "Faction"
+A CNGR group NULL, * ; connection, PLYR (in play) or NULL (not in play); note that factions represent clockwise order
+A CNNR item 0, * ; connection (player) number X if taken by player X, otherwise 0
+A NAME string(16) ; faction name
+        "Aquila", "Luna Crescens", "Parma", "Corona", "Manus", "Porta"
+
+
+G SENA 35 1 ; "Family Card / Senator"
+A CGR group ; group connection: faction (aligned), forum (unaligned), deck, null (not in play)
+        NULL, FACT, FACT, FACT, FACT, FACT, FACT, NULL, FACT, FACT, 
+        FACT, FACT, FACT, FACT, FACT, FACT, FACT, NULL, FACT, FACT, 
+        FACT, NULL, *
+A CNR item
+        0, 1, 2, 6, 3, 1, 2, 0, 2, 5, 
+        3, 1, 5, 4, 4, 3, 6, 0, 5, 4,
+        6, 0, *
+A NUM uint8_t 
+         0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  
+        10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
+        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+        30, 31, 32, 33, 34
+A NAM string(16)
+        "none",       "Cornelius" , "Fabius"    , "Valerius"  , "Julius"    , 
+        "Claudius"  , "Manlius"   , "Fulvius"   , "Furius"    , "Aurelius"  ,
+        "Junius"    , "Papirius"  , "Acilius"   , "Flaminius" , "Aelius"    , 
+        "Sulpicius" , "Calpurnius", "Plautius"  , "Quinctius" , "Aemilius"  , 
+        "Terentius" , "Servilius" , "Porcius"   , "Popillius" , "Cassius"   , 
+        "Sempronius", "Octavius"  , "Marius"    , "Tullius"   , "Licinius"  , 
+        "Pompeius"  , "Domitius"  , "Antonius"  , "Caecilius" , "Marcius"
